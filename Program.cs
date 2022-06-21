17b178307aef8fad6f979aca8157053c8aae7162
kaanyilmazer <iamyilmazer@gmail.com>
2022-06-21 19:20:53 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using SingletonDesignPattern1.SingletonDesignPattern;
+using System;
+
+namespace SingletonDesignPattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            DBHelper con1 = DBHelper.CreateDBHelper("Test Connection");
+            con1.StateMessage = "İlk Baglanti Saglandi";
+            Console.WriteLine(con1.StateMessage);
+
+            DBHelper con2 = DBHelper.CreateDBHelper("Another Test Connection");
+            con2.StateMessage = "İkinci bir baglantı saglandı";
+            Console.WriteLine(con2.StateMessage);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonDesignPattern/DBHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern1.SingletonDesignPattern
+{
+    class DBHelper
+    {
+        public string ConnectionString { get; set; }
+
+        private DBHelper(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        private static DBHelper _help;
+
+        public static DBHelper CreateDBHelper(string connectionString)
+        {
+            if (_help == null)
+            {
+                _help = new DBHelper(connectionString);
+            }
+            return _help;
+        }
+        public string StateMessage { get; set; }
+    }
+}
